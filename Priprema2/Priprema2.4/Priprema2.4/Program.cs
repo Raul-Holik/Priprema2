9bd75697699d67e403f7b7dca91d0ec0151ede1b
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-01-24 17:08:00 +0100

    Add files via upload

---
+++ Priprema2/Priprema2.4/Priprema2.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double sP, bP;
+
+            Console.WriteLine("Unesite cijenu bez PDV-a:");
+            bP = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Unesite cijenu s PDV-om:");
+            sP = Convert.ToDouble(Console.ReadLine());
+            if (sP < bP)
+                Console.WriteLine("Cijene nisu ispravno unešene");
+            else
+                Console.WriteLine("PDV iznosi: " +(sP - bP) / bP * 100);
+        }
+    }
+}
